5ed383b2bf36719a2d18af7f7e1e6ea840a12598
Raunak Kumar <kumrau@ucles.internal>
2018-03-12 17:01:29 +0530

    push

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,22 @@
+importScripts('https://www.gstatic.com/firebasejs/3.9.0/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/3.9.0/firebase-messaging.js');
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyClcOIs8mgAtSSqdbZRkm6oSKk6KG4AJmc",
+    authDomain: "push-project-177ed.firebaseapp.com",
+    databaseURL: "https://push-project-177ed.firebaseio.com",
+    projectId: "push-project-177ed",
+    storageBucket: "push-project-177ed.appspot.com",
+    messagingSenderId: "799979787181"
+  };
+  firebase.initializeApp(config);
+  
+  const messaging = firebase.messaging();
+  messaging.setBackgroundMessageHandler(function(){
+  	const title = "Hello World";
+  	const options = {
+  		body:payload.data.status
+  	}
+  	return self.registration.showNotification(title,options);
+  })
+

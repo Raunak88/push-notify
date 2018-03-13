a5f1bd877983414b3db5414dab3125b4222d65c4
Raunak Kumar <kumrau@ucles.internal>
2018-03-13 15:16:59 +0530

    push

---
+++ app.js
@@ -0,0 +1,30 @@
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyClcOIs8mgAtSSqdbZRkm6oSKk6KG4AJmc",
+    authDomain: "push-project-177ed.firebaseapp.com",
+    databaseURL: "https://push-project-177ed.firebaseio.com",
+    projectId: "push-project-177ed",
+    storageBucket: "push-project-177ed.appspot.com",
+    messagingSenderId: "799979787181"
+  };
+  window.firebase = firebase;
+  firebase.initializeApp(config);
+  
+  const messaging = firebase.messaging();
+  messaging.requestPermission()
+  .then(function(){
+    console.log('I am in here');
+    
+    return messaging.getToken()
+  .then(function(currentToken) {
+    console.log(currentToken);
+  })
+  .catch(function(err) {
+    console.log('An error occurred while retrieving token. ', err);
+    showToken('Error retrieving Instance ID token. ', err);
+    setTokenSentToServer(false);
+  });
+
+  }).catch(function(err){
+    console.log('Error');
+  });

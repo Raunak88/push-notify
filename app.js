80c9d35ec3843674e9c3fa589af11dd6251be4a8
Raunak Kumar <kumrau@ucles.internal>
2018-03-13 16:21:52 +0530

    push

--- app.js
+++ app.js
@@ -8,6 +8,61 @@
     messagingSenderId: "799979787181"
   };
   window.firebase = firebase;
-  firebase.initializeApp(config);
+  firebase.initializeApp(config);messaging.requestPermission()
+.then(function() {
+  console.log('Notification permission granted.');
+  messaging.getToken()
+  .then(function(currentToken) {
+    if (currentToken) {
+      sendTokenToServer(currentToken);
+      updateUIForPushEnabled(currentToken);
+    } else {
+      // Show permission request.
+      console.log('No Instance ID token available. Request permission to generate one.');
+      // Show permission UI.
+      updateUIForPushPermissionRequired();
+      setTokenSentToServer(false);
+    }
+  })
+  .catch(function(err) {
+    console.log('An error occurred while retrieving token. ', err);
+    showToken('Error retrieving Instance ID token. ', err);
+    setTokenSentToServer(false);
+  });
+}
+})
+.catch(function(err) {
+  console.log('Unable to get permission to notify.', err);
+});
+
+
+
+
+
   
   
+  messaging.requestPermission()
+.then(function() {
+  console.log('Notification permission granted.');
+  // TODO(developer): Retrieve an Instance ID token for use with FCM.
+  // ...
+})
+.catch(function(err) {
+  console.log('Unable to get permission to notify.', err);
+});
+  .then(function(){
+    console.log('I am in here');
+    
+    return messaging.getToken()
+  .then(function(currentToken) {
+    console.log(currentToken);
+  })
+  .catch(function(err) {
+    console.log('An error occurred while retrieving token. ', err);
+    showToken('Error retrieving Instance ID token. ', err);
+    setTokenSentToServer(false);
+  });
+
+  }).catch(function(err){
+    console.log('Error');
+  });
--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -5,13 +5,4 @@ firebase.initializeApp({
 });
 
 console.log(firebase.messaging());
-const messaging = firebase.messaging();
-messaging.requestPermission()
-.then(function() {
-  console.log('Notification permission granted.');
-  // TODO(developer): Retrieve an Instance ID token for use with FCM.
-  // ...
-})
-.catch(function(err) {
-  console.log('Unable to get permission to notify.', err);
-});
+

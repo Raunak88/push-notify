59493c8d59074b46519f4b581d947e1dd1d65728
Raunak Kumar <kumrau@ucles.internal>
2018-03-13 16:32:46 +0530

    push

--- app.js
+++ app.js
@@ -13,8 +13,12 @@
   messaging.requestPermission()
 .then(function() {
   console.log('Notification permission granted.');
-  
+  return messaging.getToken();
 })
+.then(function(token){
+    console.log(token);
+  })
+
 .catch(function(err) {
   console.log('Unable to get permission to notify.', err);
 });

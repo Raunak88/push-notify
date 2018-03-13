9034de283d029961c30b042f20c8ebdfb5e4cace
Raunak Kumar <kumrau@ucles.internal>
2018-03-13 16:29:14 +0530

    push

--- app.js
+++ app.js
@@ -13,24 +13,7 @@
   messaging.requestPermission()
 .then(function() {
   console.log('Notification permission granted.');
-  .then(function(currentToken) {
-    if (currentToken) {
-      sendTokenToServer(currentToken);
-      updateUIForPushEnabled(currentToken);
-    } else {
-      // Show permission request.
-      console.log('No Instance ID token available. Request permission to generate one.');
-      // Show permission UI.
-      updateUIForPushPermissionRequired();
-      setTokenSentToServer(false);
-    }
-  })
-  .catch(function(err) {
-    console.log('An error occurred while retrieving token. ', err);
-    showToken('Error retrieving Instance ID token. ', err);
-    setTokenSentToServer(false);
-  });
-
+  
 })
 .catch(function(err) {
   console.log('Unable to get permission to notify.', err);

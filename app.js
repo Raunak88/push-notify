a056e9d5e86f943e1fe3e2f4a219df77d478dc88
Raunak Kumar <kumrau@ucles.internal>
2018-03-16 18:29:48 +0530

    new

--- app.js
+++ app.js
@@ -1,32 +1,10 @@
-// Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyClcOIs8mgAtSSqdbZRkm6oSKk6KG4AJmc",
-    authDomain: "push-project-177ed.firebaseapp.com",
-    databaseURL: "https://push-project-177ed.firebaseio.com",
-    projectId: "push-project-177ed",
-    storageBucket: "push-project-177ed.appspot.com",
-    messagingSenderId: "799979787181"
-  };
-  window.firebase = firebase;
-  firebase.initializeApp(config);
-  const messaging = firebase.messaging();
-  messaging.requestPermission()
-.then(function() {
-  console.log('Notification permission granted.');
-  messaging.getToken()
-  .then(function(currentToken) {
-    if (currentToken) {
-            console.log(currentToken);
-    } else {
-      console.log('No Instance ID token available. Request permission to generate one.');     
-    }
-  })
-  .catch(function(err) {
-    console.log('An error occurred while retrieving token. ', err);
-  });
-})
-.catch(function(err) {
-  console.log('Unable to get permission to notify.', err);
-});
-  
-  
+
+var pubkey ="BCLnl2BFwGStqopEUJhhyK92o8QQUbdgUBMNIMbDw5hOYAS16zMN6X19a41IpsrpcbIvpyBqev6QeKenDy7-Ito";
+
+//Private Key 9E-sLIM5TH38ZprRpSOPIOpVsBDQS62jBremexnZC_g
+
+var notifyBtn = document.getElementById('bell')
+
+if('serviceWorker' in navigator && 'PushManager' in window){
+	notifyBtn.classList.remove('hidden');
+}

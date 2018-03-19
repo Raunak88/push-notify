43bb157fee5a0c8ff9f025380b22047649755bac
Raunak Kumar <kumrau@ucles.internal>
2018-03-19 16:22:04 +0530

    new

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
 console.log("Service worker running!");
 self.addEventListener('push', function(event) {
   console.log('[Service Worker] Push Received.');
-  console.log('[Service Worker] Push had this data: "${event.data.text()}"');
+  console.log(`[Service Worker] Push had this data: "${event.data.text()}"`);
 
   var title = 'Push Recived';
   var options = {
-    body: 'Your Message '+$event.data.text(),
+    body: `Your Message +"${event.data.text()}"`,
     icon: 'images/icon.png',
     badge: 'images/badge.png'
   };

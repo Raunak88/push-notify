0975d3ccb6164fb477cea13a7243c14a807845d8
Raunak Kumar <kumrau@ucles.internal>
2018-03-19 15:27:53 +0530

    new

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
 console.log("Service worker running!");
 self.addEventListener('push', function(event) {
   console.log('[Service Worker] Push Received.');
-  console.log(`[Service Worker] Push had this data: "${event.data.text()}"`);
+  console.log('[Service Worker] Push had this data: "${event.data.text()}"');
 
   var title = 'Push Codelab';
   var options = {
-    body: 'Yay it works.',
+    body: '${event.data.text()}',
     icon: 'images/icon.png',
     badge: 'images/badge.png'
   };
